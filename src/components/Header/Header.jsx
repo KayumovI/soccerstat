def3411f0bb4b78cf0bde05602afecd35b5a8290
KayumovI <ilschat98@gmail.com>
2022-09-27 12:16:51 +0400

    Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,35 @@
-import React from 'react'
-import logoBall from '../../assets/img/logoball.svg'
-import styles from './Header.module.scss'
-import { menu } from './menu'
+import { AppBar, Toolbar, Container, Typography, Tab, Tabs, Box } from '@mui/material';
+import logo from '../../assets/img/logoball.svg'
+import "./Header.css"
+import { useState } from "react"
 
 
 const Header = () => {
+  const [value, setValue] = useState('one')
+  const handleChange = (event, newValue) => {
+      setValue(newValue)
+  }
+
   return (
-    <div>
-        <div className="logo">
-          <img src={logoBall} alt="" />
-        </div>
-        <div className="menu-wrapper">
-          <ul>
-            {menu.map((item, idx) => (  
-              <li key = {`menu item ${idx}`}>
-                <a href={item.link}>{item.title}</a>
-              </li>
-              ))}
-          </ul>
-        </div>
-    </div>
+    <AppBar position="static" >
+            <Container sx={{ ml: 0}} >
+                <Toolbar>
+                  <img src={logo} className="logo" />
+                    <Typography variant="h6" sx ={{ml: 2}}>Футбольная статистика</Typography>
+                    <Box sx={{ width: '50%', ml: 1 }}>
+                        <Tabs
+                            value={value}
+                            onChange={handleChange}
+                            textColor="inherit"
+                            TabIndicatorProps={{ sx: { bgcolor: "white"} }}
+                        >
+                            <Tab value="one" label="Лиги"/>
+                            <Tab value="two" label="Команды"/>
+                        </Tabs>
+                    </Box>
+                </Toolbar>
+            </Container>
+      </AppBar>
   )
 }
 
